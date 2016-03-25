d2954f8d6efae33c076d8dc58dca963b946718de
jvidiri <joao.vidiri@gmail.com>
2016-03-25 16:43:09 -0300

    Deleções.

--- template/header.php
+++
@@ -1,4 +0,0 @@
-<?php
- /*This will be included on every new page, put here all the global code for the aplication. */ ?>
-<!-- Include do bootstrap -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
